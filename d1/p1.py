9edb069c819339f26ccfa6c5af706d7840d2b587
Bill LeBorgne <bill@leborgne.name>
2020-12-01 08:19:16 -0700

    Day 1, part 1
    Had a heck of a time until I realized the files lines were
    read in as strings, and had to be cast as ints

---
+++ d1/p1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import sys
+import os.path
+# Get input file from cmd line args
+print("Starting....")
+print("arg length: ", str(len(sys.argv)))
+
+if len(sys.argv) == 1:
+    print("Input filename required")
+    exit()
+
+input_filename = sys.argv[1]
+print("Using input file: ", input_filename)
+
+# Check if file exists
+if not os.path.isfile(input_filename):
+    print("Not a valid input file: ", input_filename)
+    exit()
+
+# Read input file into a list, casting to int
+input_file = open(input_filename, 'r')
+input_list = list(map(int, input_file.readlines()))
+input_length = len(input_list)
+print("Read " + str(input_length) + " lines from file.")
+
+# Iterate over list until two elements summing to 2020 are found
+for i in range(0, (input_length - 1)):
+    for j in range(i + 1, input_length):
+        sum = input_list[i] + input_list[j]
+        if sum == 2020:
+            # Get product of those elements
+            product = input_list[i] * input_list[j]
+            # Output
+            print("Found " + str(input_list[i]) + " * " + str(input_list[j]) + " = " + str(product))
+            exit()
+
+print("Something went wrong; no matching pairs found.")
