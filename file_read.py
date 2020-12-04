8b2906b19bdd4536cde13f402b9ae4336b10a617
Bill LeBorgne <bill@leborgne.name>
2020-12-04 07:20:02 -0700

    Base for other tasks
    All other tasks are going to need to read in a file, so
    may as well have a ready starting point

---
+++ file_read.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+import os.path
+
+# Get input file from cmd line args
+print("Starting....")
+print("arg length: ", str(len(sys.argv)))
+
+if len(sys.argv) == 1:
+    print("Input filename required")
+    exit()
+
+input_filename = sys.argv[1]
+print("Using input file: ", input_filename)
+
+# Check if file exists
+if not os.path.isfile(input_filename):
+    print("Not a valid input file: ", input_filename)
+    exit()
+
+# Read input file into a list, casting to int
+input_file = open(input_filename, 'r')
+input_list = input_file.readlines()
+input_length = len(input_list)
+print("Read " + str(input_length) + " lines from file.")
+
