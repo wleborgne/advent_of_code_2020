177c60b6daf8e4a0d0311a0896dfbdd7b9030ae7
Bill LeBorgne <bill@leborgne.name>
2020-12-09 08:41:36 -0700

    Day 9, part 2
    Probably shouldn't have to compute _all_ sums for second
    part, since any sum over our target is obviously incorrect;
    just didn't want to spend the time to do it more correctly.

---
+++ d9/p2.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+import sys
+import os.path
+import itertools
+
+# Get input file from cmd line args
+print("Starting....")
+print("arg length: ", str(len(sys.argv)))
+
+if len(sys.argv) < 3:
+    print("Missing preamble and/or filename")
+    exit()
+
+preamble_size = int(sys.argv[1])
+input_filename = sys.argv[2]
+print("Using input file: ", input_filename)
+print("with preamble value ", preamble_size)
+
+# Check if file exists
+if not os.path.isfile(input_filename):
+    print("Not a valid input file: ", input_filename)
+    exit()
+
+# Read input file into a list, casting to int
+input_file = open(input_filename, 'r')
+input_list = list(map(int, input_file.readlines()))
+input_length = len(input_list)
+print("Read " + str(input_length) + " lines from file.")
+
+target = 0
+
+for i in range(input_length):
+    current_chunk = input_list[i:i + preamble_size]
+    current_element = input_list[i + preamble_size]
+    # get all permutations of 2 elements for current chunk
+    perms = list(itertools.permutations(current_chunk, 2))
+    sums = []
+
+    # sum the tuples
+    for perm in perms:
+        sums.append(perm[0] + perm[1])
+
+    # check if current element is in the set of sums
+    if current_element in sums:
+        # if so, continue
+        next
+    else:
+        # if not, set target to current element and break
+        print("No sum found for: ", current_element)
+        target = current_element
+        break
+
+if target == 0:
+    print("Something went wrong!")
+    exit()
+
+for i in range(input_length):
+    # Get all contiguous sums starting from i
+    current_sums = list(itertools.accumulate(input_list[i:]))
+    # If our target is in that list
+    if target in current_sums:
+        # Get the index of that sum; this is the offset from
+        # initial element
+        x = current_sums.index(target)
+        # Make a sublist from i to x
+        sublist = input_list[i:i+x+1]
+        # Sort sublist
+        sublist.sort()
+        # Output sum of first and last elements of sorted list
+        print("Found ", sublist[0] + sublist[-1])
+        break
+        
+print("Done.")
+
